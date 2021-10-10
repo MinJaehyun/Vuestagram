5b75953193554b479a43a9934a6a82ee50ef0bc3
{krism01} <{krism01@naver.com}>
2021-10-10 14:12:56 +0900

    Tab 기능 구현하는 방법

--- instagram/src/App.vue
+++ instagram/src/App.vue
@@ -18,6 +18,14 @@
       <label for="file" class="input-plus">+</label>
     </ul>
   </div>
+
+  <button @click="step=0">버튼0</button>
+  <button @click="step=1">버튼1</button>
+  <button @click="step=2">버튼2</button>
+  <div v-if="step == 0">내용0</div>
+  <div v-if="step == 1">내용1</div>
+  <div v-if="step == 2">내용2</div>
+    
 </template>
 
 <script>
@@ -29,6 +37,7 @@ export default {
   name: "App",
   data() {
     return {
+      step: 0,
       Instagram: Instagram,
       Count: 0,
     }
