fa9bda9156995c2b786ff520dee523cc80a53e0b
{krism01} <{krism01@naver.com}>
2021-10-10 12:50:02 +0900

    axios 요청하여 server에서 data 가져와 post 에 더하기

--- instagram/src/App.vue
+++ instagram/src/App.vue
@@ -10,6 +10,7 @@
   </div>
 
   <Container :Instagram="Instagram"/>
+  <button @click="more">더 보기</button>
 
   <div class="footer">
     <ul class="footer-button-plus">
@@ -22,16 +23,31 @@
 <script>
 import Container from './components/Container.vue'
 import Instagram from './data/instagram.js'
+import axios from 'axios'
 
 export default {
   name: "App",
   data() {
     return {
       Instagram: Instagram,
+      Count: 0,
     }
   },
   components: {
     Container: Container,
+  },
+  methods: {
+    more(){
+      axios.get(`https://codingapple1.github.io/vue/more${this.Count++}.json`)
+      .then(result => {
+        // console.log(result.data)
+        // result.data 의 자료를 post 에 넣는다
+        this.Instagram.push(result.data)
+      })
+      .catch(err =>{
+        console.log(err)  // GET https://codingapple1.github.io/vue/more3.json 404
+      })
+    },
   },
 };
 </script>
